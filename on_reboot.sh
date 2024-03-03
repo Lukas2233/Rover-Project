29f9b950e7d0ddf3aa209564506dc2bebbbe9a68
Lukas2233 <lukasdeem@gmail.com>
2024-03-03 14:25:36 -0600

    commit

--- on_reboot.sh
+++ on_reboot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/lukas/motor-env/bin/activate
 cd /home/lukas/motor-env/Motor_Control
-python3 Rover-Project/controlmotorserial.py
+python3 /home/lukas/Rover-Project/controlmotorserial.py
