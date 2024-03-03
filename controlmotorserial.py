885ac785a9fc8b84df1075c10c6b60941a81a717
Lukas2233 <lukasdeem@gmail.com>
2024-03-02 21:53:58 -0600

    continous check for keyboard input and send to arduino

--- controlmotorserial.py
+++ controlmotorserial.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
 import serial
 import threading
+import keyboard  # Install using: pip install keyboard
 
 app = Flask(__name__)
-
+ 
 # Define the serial port and baud rate
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
@@ -14,12 +15,17 @@ def send_command(command):
 # Background thread to continuously check for commands
 def command_checker():
     while True:
-        # Check if there's a command in the request queue
-        if 'command' in request.args:
-            command = request.args['command']
-            send_command(command)
-        # Sleep for a short interval before checking again
-        time.sleep(0.1)
+        # Read keyboard inputs continuously
+        if keyboard.is_pressed('w'):
+            send_command('w')
+        elif keyboard.is_pressed('s'):
+            send_command('s')
+        elif keyboard.is_pressed('q'):
+            send_command('q')
+        elif keyboard.is_pressed('d'):
+            send_command('d')
+        elif keyboard.is_pressed('a'):
+            send_command('a')
 
 # Start the background thread
 thread = threading.Thread(target=command_checker)
